8658787315f62a74687567c08ec0b570fe8415ee
satya-vinay <satyavinay2@gmail.com>
2022-11-27 11:18:11 +0530

    added tailwind css and basic formatting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import  { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 import { useEffect, useState } from 'react';
+import FileInput from './components/FileInput';
+import Header from './components/Header'; 
 const ffmpeg = createFFmpeg({log:true});
 function App() {
   const [ready, setReady] = useState(false)
@@ -26,16 +28,21 @@ function App() {
     const url = URL.createObjectURL(new Blob([data.buffer],{type:'image/gif'}))
     setGif(url)
   }
-  return ready?(
-    <div className="App">
-      {video && <video controls width="250" src={URL.createObjectURL(video)}></video>}
-     <input type="file" onChange={(e)=>setVideo(e.target.files?.item(0))}/>
 
+  return ready?(
+    <div className="App"> 
+      <Header/>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      {video && <video controls width="250" src={URL.createObjectURL(video)}></video>}
+     <input type="file" onChange={(e)=>setVideo(e.target.files?.item(0))} className="m-2 self-center"/>
+{/* <FileInput/> */}
      <h3> Result:</h3>
-     <button onClick={convertToGif}>Convert</button>
-     {gif && <img src={gif}/>}
+     <button class="convert bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded" onClick={convertToGif}>Convert</button>
+     {gif && <img src={gif} className="m-2"/>}
+    </div>
     </div>
   ):(<p>Loading....</p>)
+  
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = ()=>{
+    return (
+        <div class="header">
+        <h2 class="text-2xl font-extrabold">Gif Maker</h2>
+        </div>
+    )
+}
+
+export default  Header
